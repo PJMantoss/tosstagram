4440aeb1567d42ec94208a6324fea7d50617bf09
PJMantoss <joelptoss@gmail.com>
2019-10-22 18:16:35 +0100

    Add stateless function to Image

---
+++ src/Image.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Image() {
+    return (
+        <div className="Image" style={{backgroundImage: 'url(' + props.image + ')'}}>
+            
+        </div>
+    )
+}
