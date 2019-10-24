05766763e9d4e690a1f49390e79d23f32954d89a
PJMantoss <joelptoss@gmail.com>
2019-10-24 19:26:12 +0100

    Add Styling to Filter and update Image

--- src/Image.js
+++ src/Image.js
@@ -11,7 +11,7 @@ export default class Image extends React.Component {
     render(){
         return (
             <div className="Image" style={{backgroundImage: 'url(' + this.props.image + ')'}}>
-                {this.props.image}
+                
             </div>
         )
     }
