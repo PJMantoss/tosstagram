158f8b6ceca40dd86d3cbd9bb372877297942287
PJMantoss <joelptoss@gmail.com>
2019-10-28 19:09:56 +0100

    Update Filter

--- src/Filter.js
+++ src/Filter.js
@@ -1,47 +1,14 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import Image from './Image';
 
-export class Filter extends Component {
-
-    // property initializer syntax
-    getFilterCSSStyles = (functions) => {
-        let filterString = "";
-         for (let filter in functions) {
-             if (functions.hasOwnProperty(filter)) {
-                 switch(filter) {
-                     case 'hue': filterString += 'hue-rotate(' + functions[filter] + ' deg)'; 
-                     break;
-                     default: filterString += filter + ' (' + functions[filter] + ' %) '
-                 }
-             }
-         }
-
-         return filterString;
-    }
-
-    render() {
-        let filterstring = this.getFilterCSSStyles(this.props.filterFunctions);
-        return (
-            <div 
-                className="Filter" 
-                style={{width:'100%', height:'100%', filter: filterstring}} 
-                onClick={()=>{this.props.onClick(this.props.filterFunctions)}}
-            >
-                {this.props.children}
-            </div>
-        )
-    }
+export default function Filter(props) {
+    return (
+        <div className="Filter" onClick={props.onClick}>
+            <Image 
+                id={'filter-' + props.id} 
+                settings={props.settings} 
+                image={props.image} 
+            />
+        </div>
+    )
 }
-
-Filter.propTypes = {
-    filterFunctions: PropTypes.shape({
-        hue: PropTypes.number,
-        contrast: PropTypes.number,
-        brightness: PropTypes.number,
-        saturate: PropTypes.number,
-        sepia: PropTypes.number
-    }),
-    onClick: PropTypes.func
-};
-
-export default Filter
