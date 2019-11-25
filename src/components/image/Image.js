53a57ba4376252aab59cd25924410173b127255a
PJMantoss <joelptoss@gmail.com>
2019-11-25 18:03:39 +0100

    Update Image component

--- src/components/image/Image.js
+++ src/components/image/Image.js
@@ -13,30 +13,6 @@ export default class Image extends React.Component {
 
     render(){
 
-        if(!this.props.settings == []){
-            
-            let filters = this.props.settings.map((filter, i) => {
-                if(filter.name == 'hue'){
-                    filterString = filterString + 'hue-rotate(' + filter.value + ')';
-                } else {
-                    filterString = filterString + filter.name + '(' + filter.value + ')';
-                }
-
-                return filterString;
-            });
-        }
-
-        const style = {
-            backgroundImage: 'url(' + this.props.image + ')',
-			webkitFilter: filterString
-        }
-
-        if(!this.props.id){
-            let id = 'filter-image';
-        } else {
-            let id = this.props.id;
-        }
-
         return (
             <div className="Image" style={{backgroundImage: 'url(' + this.props.image + ')'}}>
 
